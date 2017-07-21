7583a4d5f9140ada0ca3e2097a0da8f49ef10c62
andrew <30326421+znpp@users.noreply.github.com>
2017-07-21 17:58:21 -0400

    some improvements

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,15 @@ $(function () {
     var dateField = $('#date');
     dateField.val($.datepicker.formatDate(dateField.data('date-format'), new Date()));
 
-    window.on("load", function () {
+    $('div[data-role="footer"]').click(function () {
+        // document.body.requestFullscreen();
+        // window.scrollTo(0, 100);
+        window.scrollTo(0, 0);
+        // $('div[data-role="header"] h1').text('exec: 7_' + Math.random());
+        $('div[data-role="header"] h1').text(window.scrollY);
+    });
+
+    $(window).on("load", function () {
         // Set a timeout...
         setTimeout(function () {
             // Hide the address bar!
