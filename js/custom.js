c8e8f35037433b500739f818b959dbabc3a7005b
andrew <30326421+znpp@users.noreply.github.com>
2017-07-21 03:18:26 -0400

    make init data equals today

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,12 @@
-window.addEventListener("load", function () {
-    // Set a timeout...
-    setTimeout(function () {
-        // Hide the address bar!
-        window.scrollTo(0, 1);
-    }, 100);
+$(function () {
+    var dateField = $('#date');
+    dateField.val($.datepicker.formatDate(dateField.data('date-format'), new Date()));
+
+    window.on("load", function () {
+        // Set a timeout...
+        setTimeout(function () {
+            // Hide the address bar!
+            window.scrollTo(0, 1);
+        }, 100);
+    });
 });
