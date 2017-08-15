3da1083b79ef41ee9af94144e9daa2d3d437092c
andrew <30326421+znpp@users.noreply.github.com>
2017-08-14 22:07:39 -0400

    weekends added to headers

--- js/custom.js
+++ js/custom.js
@@ -16,28 +16,27 @@ $(function () {
 
     $field.change(function () {
         var curDate = $(this).date('getDate');
+        var dateOfFirst = new Date(curDate.getFullYear(), curDate.getMonth());
+        var dtw = (7 - dateOfFirst.getDay()) % 7; //days till weekend
         var curDay = curDate.getDate() - 1;
         var daysInMonth = $.datepicker._getDaysInMonth(curDate.getFullYear(), curDate.getMonth());
-        var shiftNum = ((Math.round((curDate - baseDate) / 8.64e7) - curDay) % 4 + 4) % 4; //'+4' for years < baseDate offset
+        var shiftNum = (Math.round((dateOfFirst - baseDate) / 8.64e7) % 4 + 4) % 4; //'+4' for years < baseDate offset
         var $realDays = $days.slice(shiftNum, shiftNum + daysInMonth);
         var $day = $realDays.eq(curDay);
 
         //clear previous values
         $cellsToHide.hide();
         $days.removeClass('selected');
+        $dayHeaders.removeClass('weekend');
 
         //set new values
         $dayHeaders.slice(0, daysInMonth).show();
+        $dayHeaders.filter(':nth-child(7n+' + dtw + '), :nth-child(7n+' + (dtw + 1) + ')').addClass('weekend');
         $realDays.show();
         $day.addClass('selected');
         $daysContainer.animate({
             scrollLeft: $day.offset().left - $daysContainer.offset().left + $daysContainer.scrollLeft()
         }, 1000);
-
-        $('.console').html("" +
-            "Shift: " + shiftNum + " <br>\n" +
-            "curDay: " + curDay + " <br>\n" +
-            "daysInM: " + daysInMonth);
     });
 
     $field.trigger('change');
