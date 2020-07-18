726a481db35df599eedb498231f5be03bdf64a03
andrew <30326421+znpp@users.noreply.github.com>
2020-07-18 05:18:03 +0300

    new type of shifts has been added

--- js/main.js
+++ js/main.js
@@ -3,10 +3,20 @@ $(function () {
     var baseDate = new Date(2000, 0);
     var $field = $('[data-role="date"]');
     var $copyright = $('[data-role="footer"] .copyright');
-    var $daysContainer = $('.days');
-    var $dayHeaders = $daysContainer.find('.headers .cell');
-    var $days = $daysContainer.find('.day');
-    var $cellsToHide = $daysContainer.find('.hide');
+    var data = [
+        {
+            $daysContainer: $('.type1 > .days')
+        },
+        {
+            $daysContainer: $('.type2 > .days')
+        }
+    ];
+
+    data.forEach(function (item) {
+        item.$dayHeaders = item.$daysContainer.find('.headers .cell');
+        item.$days = item.$daysContainer.find('.day');
+        item.$cellsToHide = item.$daysContainer.find('.hide');
+    });
 
     $('body').on('touchmove', function (e) {
         if (!$(e.target).parents('.days').length) {
@@ -14,34 +24,39 @@ $(function () {
         }
     });
 
-    $field.val($.datepicker.formatDate($field.data('date-format'), todayDate));
-
     $field.change(function () {
         var curDate = $(this).date('getDate');
         var dateOfFirst = new Date(curDate.getFullYear(), curDate.getMonth());
         var dtw = (7 - dateOfFirst.getDay()) % 7; //days till weekend
-        var curDay = curDate.getDate() - 1;
         var daysInMonth = $.datepicker._getDaysInMonth(curDate.getFullYear(), curDate.getMonth());
-        var shiftNum = (Math.round((dateOfFirst - baseDate) / 8.64e7) % 4 + 4) % 4; //'+4' for years < baseDate offset
-        var $realDays = $days.slice(shiftNum, shiftNum + daysInMonth);
-        var $day = $realDays.eq(curDay);
 
-        //clear previous values
-        $cellsToHide.hide();
-        $days.removeClass('selected');
-        $dayHeaders.removeClass('weekend');
-
-        //set new values
-        $dayHeaders.slice(0, daysInMonth).show();
-        $dayHeaders.filter(':nth-child(7n+' + dtw + '), :nth-child(7n+' + (dtw + 1) + ')').addClass('weekend');
-        $realDays.show();
-        $day.addClass('selected');
-        $daysContainer.animate({
-            scrollLeft: $day.offset().left - $daysContainer.offset().left + $daysContainer.scrollLeft()
-        }, 1000);
+        mainFunc(0, curDate, dateOfFirst, dtw, daysInMonth);
+        mainFunc(1, curDate, dateOfFirst, dtw, daysInMonth);
     });
 
-    //need for initial rendering
+    $field.val($.datepicker.formatDate($field.data('date-format'), todayDate));
     $field.trigger('change');
     $copyright.text($copyright.text() + todayDate.getFullYear());
+
+    function mainFunc(index, curDate, dateOfFirst, dtw, daysInMonth) {
+        var item = data[index];
+        var divider = index ? 4 : 15;
+        var shiftNum = (Math.round((dateOfFirst - baseDate) / 8.64e7) % divider + divider) % divider; //'+divider' for years < baseDate offset
+        var $realDays = item.$days.slice(shiftNum, shiftNum + daysInMonth);
+        var $day = $realDays.eq(curDate.getDate() - 1);
+
+        //clear previous values
+        item.$cellsToHide.hide();
+        item.$days.removeClass('selected');
+        item.$dayHeaders.removeClass('weekend');
+
+        //set new values
+        item.$dayHeaders.slice(0, daysInMonth).show();
+        item.$dayHeaders.filter(':nth-child(7n+' + dtw + '), :nth-child(7n+' + (dtw + 1) + ')').addClass('weekend');
+        $realDays.show();
+        $day.addClass('selected');
+        item.$daysContainer.animate({
+            scrollLeft: $day.offset().left - item.$daysContainer.offset().left + item.$daysContainer.scrollLeft()
+        }, 1000);
+    }
 });
